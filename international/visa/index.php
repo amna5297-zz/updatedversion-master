bb6a3a19842016ecfa8194583fd50d5f82fbb34b
amna5297 <amna.rehman@hotmail.com>
2016-09-07 14:49:18 +0500

    visa

---
+++ international/visa/index.php
@@ -0,0 +1,148 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>NIMUN '17</title>
+
+    <!-- CHANGE ignore -->
+
+    <!-- Bootstrap -->
+    <link href="/updatedversion-master/myassets/dist/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="/updatedversion-master/assets/css/custom.css" rel="stylesheet" media="screen">
+
+    <!-- Carousel -->
+	<link href="/updatedversion-master/myassets/examples/carousel/carousel.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,300,300italic' rel='stylesheet' type='text/css'>
+	<!-- Font-Awesome -->
+    <link rel="stylesheet" type="text/css" href="/updatedversion-master/assets/css/font-awesome.css" media="screen" />
+    <!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="assets/css/font-awesome-ie7.css" media="screen" /><![endif]-->
+
+    <!-- REVOLUTION BANNER CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="/updatedversion-master/myassets/css/fullscreen.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/updatedversion-master/myassets/rs-plugin/css/settings.css" media="screen" />
+
+    <!-- Picker UI-->
+	<link rel="stylesheet" href="/updatedversion-master/assets/css/jquery-ui.css" />
+
+    <!-- jQuery -->
+    <script src="/updatedversion-master/assets/js/jquery.v2.0.3.js"></script>
+
+  </head>
+  <body id="top">
+
+
+    <?php include '../../partials/navbar.php';?>
+
+    <BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR />
+
+    <div class="container cstyle08">
+
+        <div class="col-md-8">
+          <h3 class="lato bold caps size40 lblue">VISA DETAILS</h3>
+        </div>
+    </div>
+
+    <BR /><BR /><BR />
+
+    <div class="container cstyle07">
+
+        <div class="col-md-8">
+          <p class="textleft size20 grey">If you require a visa to enter Pakistan, NIMUN’16 will be able to issue an invitation letter for you. Invitation letters shall only be issued to those delegates who have completed their registration process and paid their fees in full. Please note that it is delegates’ responsibility to assert their visa status, and ensure that they apply sufficiently early. NIMUN’16 will refund fees paid by delegates who fail to obtain visas.</p>
+          <p class="textleft size20 grey">To obtain an invitation letter from NIMUN:</p>
+          <ul>
+            <li class="textleft size20 grey">Register and pay your fees.</li>
+            <li class="textleft size20 grey">Email internationallinkages.nimun16@gmail.com requesting the letter. Please attach a scanned copy of your passport photo page.</li>
+            <li class="textleft size20 grey">Email nimun16@gmail.com with the following information for the invitation letter:
+            <ul>
+              <li class="textleft size20 grey">Name (exactly as it appears on the passport):</li>
+              <li class="textleft size20 grey">Institution:</li>
+              <li class="textleft size20 grey">Passport Number:</li>
+              <li class="textleft size20 grey">Date of Issue:</li>
+              <li class="textleft size20 grey">Date of Expiry:</li>
+              <li class="textleft size20 grey">Place of Issue:</li>
+            </ul>
+            </li>
+          </ul>
+          <p class="textleft size20 grey">Once we have received this information, NIMUN will provide you with the necessary invitation letter for visa processing.</p><BR />
+          <p class="textleft size20 grey">We recommend you apply for a Standard Visitor Visa.</p>
+          <h3 class="lato bold caps size22 lblue">Applying for a Visa:</h3>
+
+          <p class="textleft size20 grey">Applying for a visa has never been easier, as NIMUN’16 is providing you with all the necessary documentation details that you’ll probably be needing to apply for a Pakistani Visa.</p><BR />
+
+          <p class="textleft size20 grey">Please note that these are the standard requirements of any visa documentation in most of the countries, and you’ll have to visit the official Pakistan High Commission websites for your respective countries.</p><BR />
+
+          <p class="textleft size20 grey">Visa processing time can vary from country to country, and can take from around 4-5 days for EU Nationals, to 1-3 weeks for general countries.</p><BR />
+
+          <h3 class="lato bold caps size22 lblue">General requirements for a Pakistani visa include:</h3>
+
+          <ul class="textleft size20 grey">
+            <li>Duly filled and signed Visa Application Form</li>
+            <li>Original passport valid for at least six months along with its photocopy</li>
+            <li>Two recent passport size photographs with white background</li>
+            <li>Sponsorship letter on Stamp Paper attested by Notary Public from Pakistan</li>
+            <li>Copy of sponsor’s Computerised National Identity Card (CNIC)</li>
+            <li>Reference letter from NIMUN’16 and from the respective institutions</li>
+            <li>Proof of residence with its photocopy</li>
+
+          </ul>
+
+        </div>
+
+        <div class="col-md-4">
+          <img src="../../myassets/images/botswana.jpg" class="fwimg">
+          <BR /><BR /><BR /><BR /><BR /><BR />
+          <img src="../../myassets/images/botswana.jpg" class="fwimg">
+        </div>
+
+
+    </div>
+
+
+    <?php include '../../partials/footer.php';?>
+
+
+
+      <!-- Javascript -->
+      <!-- This page JS -->
+  	   <script src="/updatedversion-master/assets/js/js-index.js"></script>
+
+      <!-- Custom functions -->
+      <script src="/updatedversion-master/assets/js/functions.js"></script>
+
+      <!-- Picker UI-->
+  	  <script src="/updatedversion-master/assets/js/jquery-ui.js"></script>
+
+  	   <!-- Easing -->
+      <script src="/updatedversion-master/assets/js/jquery.easing.js"></script>
+
+      <!-- jQuery KenBurn Slider  -->
+      <script type="text/javascript" src="/updatedversion-master/myassets/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+     <!-- Nicescroll  -->
+  	  <script src="/updatedversion-master/assets/js/jquery.nicescroll.min.js"></script>
+
+      <!-- CarouFredSel -->
+      <script src="/updatedversion-master/assets/js/jquery.carouFredSel-6.2.1-packed.js"></script>
+      <script src="/updatedversion-master/assets/js/helper-plugins/jquery.touchSwipe.min.js"></script>
+    	<script type="text/javascript" src="/updatedversion-master/assets/js/helper-plugins/jquery.mousewheel.min.js"></script>
+    	<script type="text/javascript" src="/updatedversion-master/assets/js/helper-plugins/jquery.transit.min.js"></script>
+    	<script type="text/javascript" src="/updatedversion-master/assets/js/helper-plugins/jquery.ba-throttle-debounce.min.js"></script>
+
+        <!-- Custom Select -->
+    	<script type='text/javascript' src='/updatedversion-master/assets/js/jquery.customSelect.js'></script>
+
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="/updatedversion-master/myassets/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- partials/navbar.php
+++ partials/navbar.php
@@ -16,111 +16,111 @@
       <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown active">
-        <a data-toggle="dropdown" class="dropdown-toggle" href="/updatedversion-master/index.html">HOME</a>
-        </li>
-        <li class="dropdown">
-        <a data-toggle="dropdown" class="dropdown-toggle" href="#">ABOUT</a>
-        <ul class="dropdown-menu posright-0">
-          <li>
-            <div class="row">
-              <ul class="droplist col-md-12">
-                <li><a href="/updatedversion-master/about/muns/index.php">MUN's</a></li>
-                <li><a href="list2.html">Tourism</a></li>
-                <li><a href="list3.html">Islamabad</a></li>
-                <li><a href="list4.html">NUST</a></li>
-              </ul>
-            </div>
+          <a data-toggle="dropdown" class="dropdown-toggle" href="/updatedversion-master/index.html">HOME</a>
           </li>
-        </ul>
-        </li>
-        <li class="dropdown">
-        <a data-toggle="dropdown" class="dropdown-toggle" href="#">NIMUN</a>
-        <ul class="dropdown-menu posleft-0">
-          <li>
-            <div class="row" style="width:150px;">
-              <ul class="droplist col-md-12">
-                <li><a href="list.html">NIMUN '14</a></li>
-                <li><a href="list2.html">NIMUN '15</a></li>
-                <li><a href="list3.html">NIMUN '16</a></li>
-              </ul>
-            </div>
+          <li class="dropdown">
+          <a data-toggle="dropdown" class="dropdown-toggle" href="#">ABOUT</a>
+          <ul class="dropdown-menu posright-0">
+            <li>
+              <div class="row">
+                <ul class="droplist col-md-12">
+                  <li><a href="/updatedversion-master/about/muns/index.php">MUN's</a></li>
+                  <li><a href="/updatedversion-master/about/tourism/index.php">Tourism</a></li>
+                  <li><a href="/updatedversion-master/about/islamabad/index.php">Islamabad</a></li>
+                  <li><a href="/updatedversion-master/about/nust/index.php">NUST</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
           </li>
-        </ul>
-        </li>
-        <li><a href="#">OUR TEAM</a></li>
-        <li class="dropdown">
-        <a data-toggle="dropdown" class="dropdown-toggle" href="#">CONFERENCE</a>
-        <ul class="dropdown-menu posleft-0">
-          <li>
-            <div class="row" style="width:650px;">
-              <ul class="droplist col-md-3">
-                <li class="dropdown-header">Details</li>
-                <li><a href="list.html">Committee Directors</a></li>
-                <li><a href="list2.html">Conference Schedule</a></li>
-                <li><a href="list3.html">Conference Fees</a></li>
-              </ul>
-              <ul class="droplist col-md-3">
-                <li class="dropdown-header">Committees</li>
-                <li><a href="list.html">General Assembly</a></li>
-                <li><a href="list2.html">Programmes and Funds</a></li>
-                <li><a href="list3.html">Regional Bodies</a></li>
-                <li><a href="list3.html">Specialized Committees</a></li>
-                <li><a href="list.html">General Assembly</a></li>
-              </ul>
-              <ul class="droplist col-md-3">
-                <li class="dropdown-header">Preparation</li>
-                <li><a>Rules of Procedure</a></li>
-                <li><a href="list.html">Guides and Downloads</a></li>
-              </ul>
-              <ul class="droplist col-md-3">
-                <li class="dropdown-header">Logistics</li>
-                <li><a>Venue</a></li>
-                <li><a href="list.html">Accomodation</a></li>
-                <li><a href="list.html">Getting There</a></li>
-              </ul>
-            </div>
+          <li class="dropdown">
+          <a data-toggle="dropdown" class="dropdown-toggle" href="#">NIMUN</a>
+          <ul class="dropdown-menu posleft-0">
+            <li>
+              <div class="row" style="width:150px;">
+                <ul class="droplist col-md-12">
+                  <li><a href="/updatedversion-master/nimun/nimun14/index.php">NIMUN '14</a></li>
+                  <li><a href="/updatedversion-master/nimun/nimun15/index.php">NIMUN '15</a></li>
+                  <li><a href="/updatedversion-master/nimun/nimun16/index.php">NIMUN '16</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
           </li>
-        </ul>
-        </li>
-        <li class="dropdown">
-        <a data-toggle="dropdown" class="dropdown-toggle" href="#">INTERNATIONAL</a>
-        <ul class="dropdown-menu posleft-0">
-          <li>
-            <div class="row" style="width:150px;">
-              <ul class="droplist col-md-12">
-                <li><a href="list.html">Visa</a></li>
-                <li><a href="login">Registration</a></li>
-                <li><a href="list3.html">Guide</a></li>
-              </ul>
-            </div>
+          <li><a href="#">OUR TEAM</a></li>
+          <li class="dropdown">
+          <a data-toggle="dropdown" class="dropdown-toggle" href="#">CONFERENCE</a>
+          <ul class="dropdown-menu posleft-0">
+            <li>
+              <div class="row" style="width:650px;">
+                <ul class="droplist col-md-3">
+                  <li class="dropdown-header">Details</li>
+                  <li><a href="/updatedversion-master/conference/details/committee-directors/index.php">Committee Directors</a></li>
+                  <li><a href="/updatedversion-master/conference/details/conference-fees/index.php">Conference Schedule</a></li>
+                  <li><a href="/updatedversion-master/conference/details/conference-schedule/index.php">Conference Fees</a></li>
+                </ul>
+                <ul class="droplist col-md-3">
+                  <li class="dropdown-header">Committees</li>
+                  <li><a href="/updatedversion-master/conference/committees/general-assembly/index.php">General Assembly</a></li>
+                  <li><a href="/updatedversion-master/conference/committees/programmes-and-funds/index.php">Programmes and Funds</a></li>
+                  <li><a href="/updatedversion-master/conference/committees/regional-bodies/index.php">Regional Bodies</a></li>
+                  <li><a href="/updatedversion-master/conference/committees/specialized-committees/index.php">Specialized Committees</a></li>
+                  <li><a href="/updatedversion-master/conference/committees/general-assembly/index.php">General Assembly</a></li>
+                </ul>
+                <ul class="droplist col-md-3">
+                  <li class="dropdown-header">Preparation</li>
+                  <li><a href="/updatedversion-master/conference/preparation/rules-of-procedure/index.php">Rules of Procedure</a></li>
+                  <li><a href="/updatedversion-master/conference/preparation/guides-and-downloads/index.php">Guides and Downloads</a></li>
+                </ul>
+                <ul class="droplist col-md-3">
+                  <li class="dropdown-header">Logistics</li>
+                  <li><a href="/updatedversion-master/conference/logistics/gettingthere/index.php">Venue</a></li>
+                  <li><a href="/updatedversion-master/conference/logistics/accomodation/index.php">Accomodation</a></li>
+                  <li><a href="/updatedversion-master/conference/logistics/venue/index.php">Getting There</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
           </li>
-        </ul>
-        </li>
-        <li class="dropdown">
-        <a data-toggle="dropdown" class="dropdown-toggle" href="#">REGISTRATION</a>
-        <ul class="dropdown-menu posleft-0">
-          <li>
-            <div class="row" style="width:500px;">
-              <ul class="droplist col-md-6">
-                <li class="dropdown-header">Registration Type</li>
-                <li>
-                  <a href="chairapplication">Chair Application</a>
-                </li>
-                <li><a href="login">Local</a></li>
-                <li><a href="login">International</a></li>
-                <li class="dropdown-header">Have an account?</li>
-                <li><a href="login">Login</a></li>
-              </ul>
-              <ul class="droplist col-md-6">
-                <li class="dropdown-header">More Details</li>
-                <li><a href="list.html">How to register</a></li>
-                <li><a href="list2.html">Registration Timeline</a></li>
-              </ul>
-            </div>
+          <li class="dropdown">
+          <a data-toggle="dropdown" class="dropdown-toggle" href="#">INTERNATIONAL</a>
+          <ul class="dropdown-menu posleft-0">
+            <li>
+              <div class="row" style="width:150px;">
+                <ul class="droplist col-md-12">
+                  <li><a href="/updatedversion-master/international/visa/index.php">Visa</a></li>
+                  <li><a href="/updatedversion-master/international/registeration/index.php">Registration</a></li>
+                  <li><a href="/updatedversion-master/international/guide/index.php">Guide</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
           </li>
-        </ul>
-        </li>
-        <li><a href="/updatedversion-master/contactus/index.php">CONTACT US</a></li>
+          <li class="dropdown">
+          <a data-toggle="dropdown" class="dropdown-toggle" href="#">REGISTRATION</a>
+          <ul class="dropdown-menu posleft-0">
+            <li>
+              <div class="row" style="width:500px;">
+                <ul class="droplist col-md-6">
+                  <li class="dropdown-header">Registration Type</li>
+                  <li>
+                    <a href="chairapplication">Chair Application</a>
+                  </li>
+                  <li><a href="login">Local</a></li>
+                  <li><a href="login">International</a></li>
+                  <li class="dropdown-header">Have an account?</li>
+                  <li><a href="login">Login</a></li>
+                </ul>
+                <ul class="droplist col-md-6">
+                  <li class="dropdown-header">More Details</li>
+                  <li><a href="list.html">How to register</a></li>
+                  <li><a href="list2.html">Registration Timeline</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+          </li>
+          <li><a href="/updatedversion-master/contactus/index.php">CONTACT US</a></li>
       </ul>
       </div>
       <!-- /Navigation-->
