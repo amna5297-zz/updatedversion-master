8fa6731c48bbb67bcb93cb625a58ae2139d3e290
amna5297 <amna.rehman@hotmail.com>
2016-09-08 22:22:32 +0500

    thori aur changes :P

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,149 +1,146 @@
 <!-- Top wrapper -->
 <div class="navbar-wrapper2 navbar-fixed-top">
-    <div class="container">
-  <div class="navbar mtnav">
+  <div class="container">
+    <div class="navbar mtnav">
 
-    <div class="container offset-3">
-      <!-- Navigation-->
-      <div class="navbar-header">
-      <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a href="index.html" class="navbar-brand"><img src="/updatedversion-master/nimun.png" alt="NIMUN '17" class="logo"/></a>
-      </div>
-      <div class="navbar-collapse collapse">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown active">
-<<<<<<< HEAD
-          <a data-toggle="dropdown" class="dropdown-toggle" href="/updatedversion-master/index.html">HOME</a>
-=======
-        <a data-toggle="dropdown" class="dropdown-toggle" href="/updatedversion-master/index.html">HOME</a>
-        </li>
-        <li class="dropdown">
-        <a data-toggle="dropdown" class="dropdown-toggle" href="#">ABOUT</a>
-        <ul class="dropdown-menu posright-0">
-          <li>
-            <div class="row">
-              <ul class="droplist col-md-12">
-                <li><a href="/updatedversion-master/about/muns/index.php">MUN's</a></li>
-                <li><a href="/updatedversion-master/about/tourism/index.php">Tourism</a></li>
-                <li><a href="list3.html">Islamabad</a></li>
-                <li><a href="list4.html">NUST</a></li>
+      <div class="container offset-3">
+        <!-- Navigation-->
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="/updatedversion-master/index.html" class="navbar-brand"><img src="/updatedversion-master/nimun.png" alt="NIMUN '17" class="logo"/></a>
+        </div>
+
+        <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="/updatedversion-master/index.html" data-toggle="dropdown" class="dropdown-toggle" >HOME</a>
+            </li>
+
+            <li class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">ABOUT</a>
+            <ul class="dropdown-menu posright-0">
+              <li>
+                <div class="row">
+                  <ul class="droplist col-md-12">
+                    <li><a href="/updatedversion-master/about/muns/index.php">MUN's</a></li>
+                    <li><a href="/updatedversion-master/about/tourism/index.php">Tourism</a></li>
+                    <li><a href="/updatedversion-master/about/islamabad/index.php">Islamabad</a></li>
+                    <li><a href="/updatedversion-master/about/nust/index.php">NUST</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+            </li>
+
+            <li class="dropdown">
+              <a data-toggle="dropdown" class="dropdown-toggle" href="#">NIMUN</a>
+              <ul class="dropdown-menu posleft-0">
+                <li>
+                  <div class="row" style="width:150px;">
+                    <ul class="droplist col-md-12">
+                      <li><a href="/updatedversion-master/nimun/nimun14/index.php">NIMUN '14</a></li>
+                      <li><a href="/updatedversion-master/nimun/nimun15/index.php">NIMUN '15</a></li>
+                      <li><a href="/updatedversion-master/nimun/nimun16/index.php">NIMUN '16</a></li>
+                    </ul>
+                  </div>
+                </li>
               </ul>
-            </div>
->>>>>>> origin/master
-          </li>
-          <li class="dropdown">
-          <a data-toggle="dropdown" class="dropdown-toggle" href="#">ABOUT</a>
-          <ul class="dropdown-menu posright-0">
-            <li>
-              <div class="row">
-                <ul class="droplist col-md-12">
-                  <li><a href="/updatedversion-master/about/muns/index.php">MUN's</a></li>
-                  <li><a href="/updatedversion-master/about/tourism/index.php">Tourism</a></li>
-                  <li><a href="/updatedversion-master/about/islamabad/index.php">Islamabad</a></li>
-                  <li><a href="/updatedversion-master/about/nust/index.php">NUST</a></li>
-                </ul>
-              </div>
             </li>
-          </ul>
-          </li>
-          <li class="dropdown">
-          <a data-toggle="dropdown" class="dropdown-toggle" href="#">NIMUN</a>
-          <ul class="dropdown-menu posleft-0">
-            <li>
-              <div class="row" style="width:150px;">
-                <ul class="droplist col-md-12">
-                  <li><a href="/updatedversion-master/nimun/nimun14/index.php">NIMUN '14</a></li>
-                  <li><a href="/updatedversion-master/nimun/nimun15/index.php">NIMUN '15</a></li>
-                  <li><a href="/updatedversion-master/nimun/nimun16/index.php">NIMUN '16</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-          </li>
+
+
           <li><a href="#">OUR TEAM</a></li>
+
+
           <li class="dropdown">
-          <a data-toggle="dropdown" class="dropdown-toggle" href="#">CONFERENCE</a>
-          <ul class="dropdown-menu posleft-0">
-            <li>
-              <div class="row" style="width:650px;">
-                <ul class="droplist col-md-3">
-                  <li class="dropdown-header">Details</li>
-                  <li><a href="/updatedversion-master/conference/details/committee-directors/index.php">Committee Directors</a></li>
-                  <li><a href="/updatedversion-master/conference/details/conference-fees/index.php">Conference Schedule</a></li>
-                  <li><a href="/updatedversion-master/conference/details/conference-schedule/index.php">Conference Fees</a></li>
-                </ul>
-                <ul class="droplist col-md-3">
-                  <li class="dropdown-header">Committees</li>
-                  <li><a href="/updatedversion-master/conference/committees/general-assembly/index.php">General Assembly</a></li>
-                  <li><a href="/updatedversion-master/conference/committees/programmes-and-funds/index.php">Programmes and Funds</a></li>
-                  <li><a href="/updatedversion-master/conference/committees/regional-bodies/index.php">Regional Bodies</a></li>
-                  <li><a href="/updatedversion-master/conference/committees/specialized-committees/index.php">Specialized Committees</a></li>
-                  <li><a href="/updatedversion-master/conference/committees/general-assembly/index.php">General Assembly</a></li>
-                </ul>
-                <ul class="droplist col-md-3">
-                  <li class="dropdown-header">Preparation</li>
-                  <li><a href="/updatedversion-master/conference/preparation/rules-of-procedure/index.php">Rules of Procedure</a></li>
-                  <li><a href="/updatedversion-master/conference/preparation/guides-and-downloads/index.php">Guides and Downloads</a></li>
-                </ul>
-                <ul class="droplist col-md-3">
-                  <li class="dropdown-header">Logistics</li>
-                  <li><a href="/updatedversion-master/conference/logistics/gettingthere/index.php">Venue</a></li>
-                  <li><a href="/updatedversion-master/conference/logistics/accomodation/index.php">Accomodation</a></li>
-                  <li><a href="/updatedversion-master/conference/logistics/venue/index.php">Getting There</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">CONFERENCE</a>
+            <ul class="dropdown-menu posleft-0">
+              <li>
+                <div class="row" style="width:650px;">
+                  <ul class="droplist col-md-3">
+                    <li class="dropdown-header">Details</li>
+                    <li><a href="/updatedversion-master/conference/details/committee-directors/index.php">Committee Directors</a></li>
+                    <li><a href="/updatedversion-master/conference/details/conference-fees/index.php">Conference Schedule</a></li>
+                    <li><a href="/updatedversion-master/conference/details/conference-schedule/index.php">Conference Fees</a></li>
+                  </ul>
+                  <ul class="droplist col-md-3">
+                    <li class="dropdown-header">Committees</li>
+                    <li><a href="/updatedversion-master/conference/committees/general-assembly/index.php">General Assembly</a></li>
+                    <li><a href="/updatedversion-master/conference/committees/programmes-and-funds/index.php">Programmes and Funds</a></li>
+                    <li><a href="/updatedversion-master/conference/committees/regional-bodies/index.php">Regional Bodies</a></li>
+                    <li><a href="/updatedversion-master/conference/committees/specialized-committees/index.php">Specialized Committees</a></li>
+                    <li><a href="/updatedversion-master/conference/committees/general-assembly/index.php">General Assembly</a></li>
+                  </ul>
+                  <ul class="droplist col-md-3">
+                    <li class="dropdown-header">Preparation</li>
+                    <li><a href="/updatedversion-master/conference/preparation/rules-of-procedure/index.php">Rules of Procedure</a></li>
+                    <li><a href="/updatedversion-master/conference/preparation/guides-and-downloads/index.php">Guides and Downloads</a></li>
+                  </ul>
+                  <ul class="droplist col-md-3">
+                    <li class="dropdown-header">Logistics</li>
+                    <li><a href="/updatedversion-master/conference/logistics/gettingthere/index.php">Venue</a></li>
+                    <li><a href="/updatedversion-master/conference/logistics/accomodation/index.php">Accomodation</a></li>
+                    <li><a href="/updatedversion-master/conference/logistics/venue/index.php">Getting There</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
           </li>
+
+
           <li class="dropdown">
-          <a data-toggle="dropdown" class="dropdown-toggle" href="#">INTERNATIONAL</a>
-          <ul class="dropdown-menu posleft-0">
-            <li>
-              <div class="row" style="width:150px;">
-                <ul class="droplist col-md-12">
-                  <li><a href="/updatedversion-master/international/visa/index.php">Visa</a></li>
-                  <li><a href="/updatedversion-master/international/registeration/index.php">Registration</a></li>
-                  <li><a href="/updatedversion-master/international/guide/index.php">Guide</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">INTERNATIONAL</a>
+            <ul class="dropdown-menu posleft-0">
+              <li>
+                <div class="row" style="width:150px;">
+                  <ul class="droplist col-md-12">
+                    <li><a href="/updatedversion-master/international/visa/index.php">Visa</a></li>
+                    <li><a href="/updatedversion-master/international/registeration/index.php">Registration</a></li>
+                    <li><a href="/updatedversion-master/international/guide/index.php">Guide</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
           </li>
+
+
           <li class="dropdown">
-          <a data-toggle="dropdown" class="dropdown-toggle" href="#">REGISTRATION</a>
-          <ul class="dropdown-menu posleft-0">
-            <li>
-              <div class="row" style="width:500px;">
-                <ul class="droplist col-md-6">
-                  <li class="dropdown-header">Registration Type</li>
-                  <li>
-                    <a href="chairapplication">Chair Application</a>
-                  </li>
-                  <li><a href="login">Local</a></li>
-                  <li><a href="login">International</a></li>
-                  <li class="dropdown-header">Have an account?</li>
-                  <li><a href="login">Login</a></li>
-                </ul>
-                <ul class="droplist col-md-6">
-                  <li class="dropdown-header">More Details</li>
-                  <li><a href="list.html">How to register</a></li>
-                  <li><a href="list2.html">Registration Timeline</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">REGISTRATION</a>
+            <ul class="dropdown-menu posleft-0">
+              <li>
+                <div class="row" style="width:500px;">
+                  <ul class="droplist col-md-6">
+                    <li class="dropdown-header">Registration Type</li>
+                    <li>
+                      <a href="/updatedversion-master/chairapplication/index.php">Chair Application</a>
+                    </li>
+                    <li><a href="login">Local</a></li>
+                    <li><a href="login">International</a></li>
+                    <li class="dropdown-header">Have an account?</li>
+                    <li><a href="login">Login</a></li>
+                  </ul>
+                  <ul class="droplist col-md-6">
+                    <li class="dropdown-header">More Details</li>
+                    <li><a href="list.html">How to register</a></li>
+                    <li><a href="list2.html">Registration Timeline</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
           </li>
+
           <li><a href="/updatedversion-master/contactus/index.php">CONTACT US</a></li>
-      </ul>
+
+        </ul>
       </div>
       <!-- /Navigation-->
-    </div>
-
       </div>
-    </div>
+
   </div>
+  </div>
+
+</div>
 <!-- /Top wrapper -->
