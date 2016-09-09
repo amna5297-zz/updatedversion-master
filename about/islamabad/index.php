39534621de6e52b37dff5a5ad1f3fb29ebced31a
Ureb <urebhasan.95@gmail.com>
2016-09-10 01:30:45 +0500

    About sub tabs

---
+++ about/islamabad/index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>NIMUN '17</title>
+
+    <!-- Bootstrap -->
+    <link href="../../myassets/dist/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="../../assets/css/custom.css" rel="stylesheet" media="screen">
+
+    <!-- Carousel -->
+	<link href="../../myassets/examples/carousel/carousel.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,300,300italic' rel='stylesheet' type='text/css'>
+	<!-- Font-Awesome -->
+    <link rel="stylesheet" type="text/css" href="../../assets/css/font-awesome.css" media="screen" />
+    <!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="assets/css/font-awesome-ie7.css" media="screen" /><![endif]-->
+
+    <!-- REVOLUTION BANNER CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="../../myassets/css/fullscreen.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="../../myassets/rs-plugin/css/settings.css" media="screen" />
+
+    <!-- Picker UI-->
+	<link rel="stylesheet" href="../../assets/css/jquery-ui.css" />
+
+    <!-- jQuery -->
+    <script src="../../assets/js/jquery.v2.0.3.js"></script>
+<style>
+body{
+
+}
+
+
+.content {
+  overflow: hidden;
+  width: 80%;
+  margin: auto;
+}
+
+.main-content{
+  background-color: rgba(0, 0, 0, 0.7);
+  overflow: hidden;
+  margin:2px;
+  padding: 10px;
+  color: #eee;
+}
+.content img{
+  width:100%;
+  height: 300px;
+}
+
+.title {
+  text-align:center;
+  margin-left: 50px;
+  margin-right:50px;
+  margin-bottom: 15px;
+}
+
+
+@media screen and (max-width: 480px) {
+    .content {
+      width: 95%;
+    }
+}
+
+</style>
+  </head>
+  <body id="top">
+
+    <?php include '../../partials/navbar.php';?>
+
+
+
+    <BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR />
+
+
+
+    <div class="main-content">
+
+      <div class="title">
+        <h2 class="lato bold caps size40 lblue">Islamabad</h2>
+        <h4>Land of serenity</h4>
+        <hr>
+      </div>
+
+
+
+
+
+      <!-- row1 -->
+      <div class="row content">
+        <div class="col-sm-7 col-xs-12">
+          <p>The so-called twin cities of Islamabad and Rawalpindi are commonly viewed as one unit, and indeed, one day the two will
+              physically merge. However, these twins, with separate ancestry and distinct personalities, are far from identical.
+              Islamabad is a late-20th-century capital laid out in straight lines and right angles: a proud metric showcase of government
+              and administration. Rawalpindi, on the other hand, grew from a ramshackle backwater village to a sprawling hub on the Grand
+              Trunk Rd during the 19th century.
+              <br>The twins’ personalities are rather like chalk and cheese: Islamabad is patently more subdued and suburban with broad
+              avenues, grassy parkland, shiny restaurants and just a whiff of the exasperating human and mechanical crush that
+              epitomises most subcontinental cities. For those with a penchant for the adrenaline-pumping hullabaloo that a typical
+              South Asian metropolis delivers, all that awaits in Rawalpindi – affectionately dubbed ‘Pindi’ – a mere 15km away.
+              <br>Nestled in the heart of Pakistan, the Federal Capital of Pakistan – Islamabad, namely – has been the centre of talk
+              and domestic tourism for over many years. Starting off as an expedition of General Ayub Khan, the city was brought to life,
+              and over the course of its existence, it has grown to become one of the most beautiful capital cities of Asia – no doubt, it
+              doesn’t hold back in the global comparison, but we’ll stick to what we know.
+            </p>
+
+        </div>
+        <div class="col-sm-5 col-xs-12">
+              <img src="../../myassets/images/botswana.jpg"/>
+        </div>
+      </div>
+
+      <br><br>
+      <!-- row2 -->
+      <div class="row content">
+        <div class="col-sm-7 col-xs-12">
+          <p>Whether it’s the fresh air and the liveliness of the place, or whether you’re a fan of amazing food, served by spectacular
+            cuisines with beautiful views such as the Monal Restaurant or famous Chaaye Tea houses like Chaayé Khana, , or any other franchise
+            you spotted in some other city; whether it’s the crowd at the amazing Centaurus Mall that takes you by surprise, or whether it’s the
+            system that gives you a sense of peace, Islamabad has it all!
+
+            <br>But it’s not just the eateries and the liveliness that makes Islamabad a popular tourist destination. The serenity of the city
+            as a whole is what defines the Federal Capital. The city is nestled in the heart of the beautiful mountains of the Pakistan, and
+            Islamabad is basically the ‘transit’ city when you’re travelling to the beautiful Northern mountainous areas. It’s a beautiful
+            morning every day, and it’s a beautiful city to live in. In all, it’s the beauty of the place that takes you out every day for a
+            stroll.
+
+            <br>Enter Islamabad, and you’ll discover the amazing Education City of NUST at the start. This 707-acre University has been the
+            proud maker of dreams for thousands of students currently enrolled at NUST. The quality of education and the vast opportunities
+            that it provides makes NUST a dream university for many to study at, which is the only Pakistani university to be ranked amongst
+            top 100 on the QS University Ranking, worldwide.
+
+            <br>Witness it all, this January, at the NUST International Model United Nations conference 2016!
+            </p>
+
+        </div>
+        <div class="col-sm-5 col-xs-12">
+              <img src="../../myassets/images/botswana.jpg"/>
+        </div>
+      </div>
+</div>
+
+
+
+
+    <?php include '../../partials/footer.php';?>
+
+
+
+      <!-- Javascript -->
+      <!-- This page JS -->
+  	   <script src="../../assets/js/js-index.js"></script>
+
+      <!-- Custom functions -->
+      <script src="../../assets/js/functions.js"></script>
+
+      <!-- Picker UI-->
+  	  <script src="../../assets/js/jquery-ui.js"></script>
+
+  	   <!-- Easing -->
+      <script src="../../assets/js/jquery.easing.js"></script>
+
+      <!-- jQuery KenBurn Slider  -->
+      <script type="text/javascript" src="../../myassets/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+     <!-- Nicescroll  -->
+  	  <script src="../../assets/js/jquery.nicescroll.min.js"></script>
+
+      <!-- CarouFredSel -->
+      <script src="../../assets/js/jquery.carouFredSel-6.2.1-packed.js"></script>
+      <script src="../../assets/js/helper-plugins/jquery.touchSwipe.min.js"></script>
+    	<script type="text/javascript" src="../../assets/js/helper-plugins/jquery.mousewheel.min.js"></script>
+    	<script type="text/javascript" src="../../assets/js/helper-plugins/jquery.transit.min.js"></script>
+    	<script type="text/javascript" src="../../assets/js/helper-plugins/jquery.ba-throttle-debounce.min.js"></script>
+
+        <!-- Custom Select -->
+    	<script type='text/javascript' src='../../assets/js/jquery.customSelect.js'></script>
+
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="../../myassets/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ about/nust/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>NIMUN '17</title>
+
+    <!-- Bootstrap -->
+    <link href="../../myassets/dist/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="../../assets/css/custom.css" rel="stylesheet" media="screen">
+
+    <!-- Carousel -->
+	<link href="../../myassets/examples/carousel/carousel.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,300,300italic' rel='stylesheet' type='text/css'>
+	<!-- Font-Awesome -->
+    <link rel="stylesheet" type="text/css" href="../../assets/css/font-awesome.css" media="screen" />
+    <!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="assets/css/font-awesome-ie7.css" media="screen" /><![endif]-->
+
+    <!-- REVOLUTION BANNER CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="../../myassets/css/fullscreen.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="../../myassets/rs-plugin/css/settings.css" media="screen" />
+
+    <!-- Picker UI-->
+	<link rel="stylesheet" href="../../assets/css/jquery-ui.css" />
+
+    <!-- jQuery -->
+    <script src="../../assets/js/jquery.v2.0.3.js"></script>
+<style>
+body{
+
+}
+
+
+.content {
+  overflow: hidden;
+  width: 80%;
+  margin: auto;
+}
+
+.main-content{
+  overflow: hidden;
+  margin:2px;
+  padding: 10px;
+  color: #aaa;
+}
+.content img{
+  width:100%;
+  height: 250px;
+}
+
+.title {
+  text-align:center;
+  margin-left: 50px;
+  margin-right:50px;
+  margin-bottom: 15px;
+}
+
+
+@media screen and (max-width: 480px) {
+    .content {
+      width: 95%;
+    }
+}
+
+</style>
+  </head>
+  <body id="top">
+
+    <?php include '../../partials/navbar.php';?>
+
+
+
+    <BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR />
+
+
+
+    <div class="main-content">
+
+      <div class="title">
+        <h2 class="lato bold caps size40 lblue">NUST</h2>
+        <h4>Quest of a dream</h4>
+        <hr>
+      </div>
+
+
+
+
+
+      <!-- row1 -->
+      <div class="row content">
+        <div class="col-sm-7 col-xs-12">
+          <p>Founded in 1991, National University of Sciences and Technology (NUST) has gained global acclaim in a very short period of time.
+            The main campus is spread over 707 acres (2.86 km2) of land with sports grounds along with various centers and schools producing
+            extraordinary research work.
+
+            <br><br>The Centre for International Peace and Stability (CIPS), inaugurated by the UN secretary general Ban Ki-Moon promises to be an
+            independent think-thank for peace and stability, development, conflict resolution, regional studies and peacekeeping research.
+            </p>
+
+        </div>
+        <div class="col-sm-5 col-xs-12">
+              <img src="../../myassets/images/botswana.jpg"/>
+        </div>
+      </div>
+
+      <br><br>
+      <!-- row2 -->
+      <div class="row content">
+        <div class="col-sm-7 col-xs-12">
+          <p>Besides constant efforts to make valuable contributions in the areas of International Relations and peace and conflict studies,
+            NUST prides itself on having its own supercomputers and state-of-the art facilities on campus. Students displaying dynamic literary,
+            creative and artistic abilities and an aptitude for co-curricular activities, demonstrates the fact that NUST is undoubtedly one of
+            the best educational institutes in the country.
+
+            <br><br>It is here where the conference will be taking place. See you in January!
+            </p>
+
+        </div>
+        <div class="col-sm-5 col-xs-12">
+              <img src="../../myassets/images/botswana.jpg"/>
+        </div>
+      </div>
+</div>
+
+
+
+
+    <?php include '../../partials/footer.php';?>
+
+
+
+      <!-- Javascript -->
+      <!-- This page JS -->
+  	   <script src="../../assets/js/js-index.js"></script>
+
+      <!-- Custom functions -->
+      <script src="../../assets/js/functions.js"></script>
+
+      <!-- Picker UI-->
+  	  <script src="../../assets/js/jquery-ui.js"></script>
+
+  	   <!-- Easing -->
+      <script src="../../assets/js/jquery.easing.js"></script>
+
+      <!-- jQuery KenBurn Slider  -->
+      <script type="text/javascript" src="../../myassets/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+     <!-- Nicescroll  -->
+  	  <script src="../../assets/js/jquery.nicescroll.min.js"></script>
+
+      <!-- CarouFredSel -->
+      <script src="../../assets/js/jquery.carouFredSel-6.2.1-packed.js"></script>
+      <script src="../../assets/js/helper-plugins/jquery.touchSwipe.min.js"></script>
+    	<script type="text/javascript" src="../../assets/js/helper-plugins/jquery.mousewheel.min.js"></script>
+    	<script type="text/javascript" src="../../assets/js/helper-plugins/jquery.transit.min.js"></script>
+    	<script type="text/javascript" src="../../assets/js/helper-plugins/jquery.ba-throttle-debounce.min.js"></script>
+
+        <!-- Custom Select -->
+    	<script type='text/javascript' src='../../assets/js/jquery.customSelect.js'></script>
+
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="../../myassets/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
