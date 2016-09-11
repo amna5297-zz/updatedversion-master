e79ad3708351826d557ed8cdf6fb08bd97011dd4
amna5297 <amna.rehman@hotmail.com>
2016-09-11 23:35:54 +0500

    slider

---
+++ index.php
@@ -0,0 +1,195 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>NIMUN '17</title>
+
+    <!-- CHANGE ignore -->
+
+    <!-- Bootstrap -->
+    <link href="myassets/dist/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="assets/css/custom.css" rel="stylesheet" media="screen">
+
+    <!-- Carousel -->
+	<link href="myassets/examples/carousel/carousel.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,400,300,300italic' rel='stylesheet' type='text/css'>
+	<!-- Font-Awesome -->
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css" media="screen" />
+    <!--[if lt IE 7]><link rel="stylesheet" type="text/css" href="assets/css/font-awesome-ie7.css" media="screen" /><![endif]-->
+
+    <link rel="stylesheet" type="text/css" href="myassets/css/slider.css" media="screen" />
+
+    <!-- REVOLUTION BANNER CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="myassets/css/fullscreen.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="myassets/rs-plugin/css/settings.css" media="screen" />
+
+    <!-- Picker UI-->
+	<link rel="stylesheet" href="assets/css/jquery-ui.css" />
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery.v2.0.3.js"></script>
+
+    <script src="myassets/js/slider/jquery-1.11.0.min.js"></script>
+    <script src="myassets/js/slider/DiagonalSlider.js"></script>
+
+  </head>
+
+  <body id="top">
+	<!-- Top wrapper -->
+
+  <?php include 'partials/navbar.php';?>
+
+	<!-- /Top wrapper -->
+<BR /><BR /><BR /><BR /><BR />
+
+        <div class="content_slider">
+          <div class="gallery_content">
+            <div class="gallery_item">
+              <img src="/updatedversion-master/myassets/images/slider/NIMUN15.png" data-title="Image 1" width="1800px" height="300px"/>
+            </div>
+            <div class="gallery_item">
+              <img src="/updatedversion-master/myassets/images/slider/NIMUN16.png" data-title="Image 2" width="1800px" height="300px"/>
+            </div>
+            <div class="gallery_item">
+              <img src="/updatedversion-master/myassets/images/slider/NIMUN15.png" data-title="Image 3" width="1800px" height="300px"/>
+            </div>
+            <div class="gallery_item">
+              <img src="/updatedversion-master/myassets/images/slider/NIMUN16.png" data-title="Image 4" width="1800px" height="300px"/>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+    <!-- WRAP -->
+    	<div class="wrap bgfix cstyle03">
+
+
+    		<div class="lastminute lcfix">
+    			<div style="margin-top:-25px;" class="container lmc">
+    				<b>ABOUT NIMUN</b><br/>
+            <span>Some description about NIMUN...</span>
+    				<form action="details.html">
+    					<button class="btn iosbtn" type="submit">Read more</button>
+    				</form>
+    			</div>
+    		</div>
+        <div class="clearfix">
+
+        </div>
+<BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR /><BR />
+    		<div class="container cstyle07">
+
+    			  <div class="col-md-4">
+    				<div class="boxshadow center">
+    					<a href="list.html"><img src="myassets/images/botswana.jpg" alt="" class="fwimg"/></a>
+    					<div class="bscontainer">
+    						<h4 class="lato bold caps">Committees</h4>
+    						<hr>
+    						<p class="textleft grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis enim et mauris bibendum lobortis eget non eros. Nulla facilisi. Nulla facilisi.</p>
+    						<br/>
+    						<form action="list4.html">
+    							<button class="roundbtn" type="submit">More</button>
+    						</form>
+    					</div>
+    				</div>
+    			  </div>
+    			  <div class="col-md-4">
+    				<div class="boxshadow center">
+    					<a href="list3.html"><img src="myassets/images/egypt.jpg" alt="" class="fwimg"/></a>
+    					<div class="bscontainer">
+    						<h4 class="lato bold caps">Conference</h4>
+    						<hr>
+    						<p class="textleft grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis enim et mauris bibendum lobortis eget non eros. Nulla facilisi. Nulla facilisi.</p>
+    						<br/>
+    						<form action="list4.html">
+    							<button class="roundbtn" type="submit">More</button>
+    						</form>
+    					</div>
+    				</div>
+    			  </div>
+    			  <div class="col-md-4">
+    				<div class="boxshadow center">
+    					<a href="list4.html"><img src="myassets/images/machu_picchu.jpg" alt="" class="fwimg"/></a>
+    					<div class="bscontainer">
+    						<h4 class="lato bold caps">International</h4>
+    						<hr>
+    						<p class="textleft grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis enim et mauris bibendum lobortis eget non eros. Nulla facilisi. Nulla facilisi.</p>
+    						<br/>
+    						<form action="list4.html">
+    							<button class="roundbtn" type="submit">More</button>
+    						</form>
+    					</div>
+    				</div>
+    			  </div>
+
+
+    		</div>
+
+
+
+
+
+
+
+    		<!-- FOOTER -->
+
+    		<?php include 'partials/footer.php';?>
+
+
+
+
+
+    	</div>
+    	<!-- WRAP -->
+
+    <!--  CODEPEN SCRIPTS -->
+    <script src="myassets/js/slider/script.js"></script>
+
+
+    <!-- Javascript -->
+    <!-- This page JS -->
+	<script src="assets/js/js-index.js"></script>
+
+    <!-- Custom functions -->
+    <script src="assets/js/functions.js"></script>
+
+    <!-- Picker UI-->
+	<script src="assets/js/jquery-ui.js"></script>
+
+	<!-- Easing -->
+    <script src="assets/js/jquery.easing.js"></script>
+
+    <!-- jQuery KenBurn Slider  -->
+    <script type="text/javascript" src="myassets/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+   <!-- Nicescroll  -->
+	<script src="assets/js/jquery.nicescroll.min.js"></script>
+
+    <!-- CarouFredSel -->
+    <script src="assets/js/jquery.carouFredSel-6.2.1-packed.js"></script>
+    <script src="assets/js/helper-plugins/jquery.touchSwipe.min.js"></script>
+	<script type="text/javascript" src="assets/js/helper-plugins/jquery.mousewheel.min.js"></script>
+	<script type="text/javascript" src="assets/js/helper-plugins/jquery.transit.min.js"></script>
+	<script type="text/javascript" src="assets/js/helper-plugins/jquery.ba-throttle-debounce.min.js"></script>
+
+    <!-- Custom Select -->
+	<script type='text/javascript' src='assets/js/jquery.customSelect.js'></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="myassets/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
