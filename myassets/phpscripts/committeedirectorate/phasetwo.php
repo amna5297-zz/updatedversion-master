d87d4953f333b60878bd5430784946b600b5caf5
amna5297 <amna.rehman@hotmail.com>
2016-09-17 04:39:29 +0500

    ignore

--- myassets/phpscripts/committeedirectorate/phasetwo.php
+++ myassets/phpscripts/committeedirectorate/phasetwo.php
@@ -15,7 +15,7 @@
     $stmt = "UPDATE CommitteeDirectorate set pastexperience = '$pastexperience' , interest = '$interest' , pastleadership = '$pastleadership'  where email = '$email'";
     //  $stmt->execute();
 
-    //$conn->query($stmt);
+    $conn->query($stmt);
 
 
     $conn->close();
